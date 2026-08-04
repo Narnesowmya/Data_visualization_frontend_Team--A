5c55001c8e265760560fc80cb5f6d8bc75dd07b6
Navya-0804 <navyasri.4664@gmail.com>
2026-08-04 22:01:38 +0530

    Added Event Table component for Task 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import EventTable from "./components/EventTable";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-  )
+    <div>
+      <EventTable />
+    </div>
+  );
 }
 
-export default App
+export default App;
